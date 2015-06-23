4144951ab326331a675f578bd6be1f03f8f2160c
Luuk Veenis <luukveenis@gmail.com>
2015-06-23 15:17:23 -0700

    Add basic checklists controller
    Implements an index action and provides a blank template for the index
    view

---
+++ LSCS.Web/Controllers/ChecklistController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LSCS.Web.Controllers
+{
+    [RoutePrefix("checklists")]
+    public class ChecklistController : Controller
+    {
+        [Route]
+        [HttpGet]
+        [AllowAnonymous]
+        // GET: Checklist
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
