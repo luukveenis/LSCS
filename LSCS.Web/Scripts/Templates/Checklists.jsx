df62dd091ebff49d3f05a250c95407a7aed7c7d6
Luuk Veenis <luukveenis@gmail.com>
2015-07-15 13:07:19 -0700

    Expand tabs to spaces
    Forgot to set global config for this, so JSX files were tabbed since there
    is no syntax support for it.

--- LSCS.Web/Scripts/Templates/Checklists.jsx
+++ LSCS.Web/Scripts/Templates/Checklists.jsx
@@ -1,44 +1,44 @@
 ﻿var Checklist = React.createClass({
-	render: function() {
-		return (
-			<tr>
-				<td>{this.props.data.Title}</td>
-				<td>{this.props.data.Description}</td>
-				<td>{this.props.data.FileNumber}</td>
-				<td>{this.props.data.Location}</td>
-			</tr>
-		);
-	}
+    render: function() {
+        return (
+            <tr>
+                <td>{this.props.data.Title}</td>
+                <td>{this.props.data.Description}</td>
+                <td>{this.props.data.FileNumber}</td>
+                <td>{this.props.data.Location}</td>
+            </tr>
+        );
+    }
 });
 
 var ChecklistList = React.createClass({
-	render: function() {
-		var checklistNodes = this.props.data.map(function (checklist) {
-			return (
-				<Checklist data={checklist} />
-			);
-		});
-		return (
-			<table>
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Description</th>
-						<th>File No.</th>
-						<th>Location</th>
-					</tr>
-				</thead>
-				<tbody>
-					{checklistNodes}
-				</tbody>
-			</table>
-		);
-	}
+    render: function() {
+        var checklistNodes = this.props.data.map(function (checklist) {
+            return (
+                <Checklist data={checklist} />
+            );
+        });
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>File No.</th>
+                        <th>Location</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {checklistNodes}
+                </tbody>
+            </table>
+        );
+    }
 });
 
 var data = [ { Title: "Test Title", Description: "Some stuff in here", FileNumber: 2053, Location: "The Moon" } ]
 
 React.render(
-	<ChecklistList data={data} />,
-	document.getElementById('checklist-list')
+    <ChecklistList data={data} />,
+    document.getElementById('checklist-list')
 );
