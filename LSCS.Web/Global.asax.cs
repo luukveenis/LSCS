fe31d02cef104ad6d10b65c5af358cb7715b3e9b
Stephen Bos <sjlbos@outlook.com>
2015-08-07 13:20:07 -0700

    Added roles and missing registration model fields.

--- LSCS.Web/Global.asax.cs
+++ LSCS.Web/Global.asax.cs
@@ -2,6 +2,9 @@
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Optimization;
+using LSCS.Web.Authentication;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace LSCS.Web
 {
@@ -11,6 +14,14 @@ namespace LSCS.Web
         {
             RouteTable.Routes.MapMvcAttributeRoutes();
             RegisterBundles(BundleTable.Bundles);
+            CreateUserRoles();
+        }
+
+        private static void CreateUserRoles()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(UserDbContext.Create()));
+            roleManager.Create(new IdentityRole("Administrator"));
+            roleManager.Create(new IdentityRole("Surveyor"));
         }
 
         private static void RegisterBundles(BundleCollection bundles)
--- LSCS.Web/Models/AccountManagementModels.cs
+++ LSCS.Web/Models/AccountManagementModels.cs
@@ -10,6 +10,14 @@ namespace LSCS.Web.Models
         public string Email { get; set; }
 
         [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
@@ -19,6 +27,10 @@ namespace LSCS.Web.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Is Administrator")]
+        public bool IsAdministrator { get; set; }
     }
 
     public class ResetPasswordViewModel
